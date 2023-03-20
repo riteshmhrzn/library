12a99880cbb8ac0fda9b45bfcd23e8a6eaaf204a
riteshmhrzn <maharjanritz@gmail.com>
2023-03-20 15:44:39 +0545

    field added in addbook form

--- force-app/main/default/lwc/addBooks/addBooks.js
+++ force-app/main/default/lwc/addBooks/addBooks.js
@@ -5,11 +5,12 @@ import BOOKNAME_FIELD from '@salesforce/schema/Book__c.Name';
 import AUTHOR_FIELD from '@salesforce/schema/Book__c.Author__c';
 import QUANTITY_FIELD from '@salesforce/schema/Book__c.Quantity__c';
 import IMAGEURL_FIELD from '@salesforce/schema/Book__c.Image_URL__c';
+import DESCRIPTION_FIELD from '@salesforce/schema/Book__c.Description__c';
 import createAccount from '@salesforce/apex/CreateBook.addBook';
 
 export default class CreateAccountRecord extends LightningElement {
     objectApiName=Book_OBJECT;
-    fields = [BOOKNAME_FIELD,AUTHOR_FIELD,QUANTITY_FIELD,IMAGEURL_FIELD];
+    fields = [BOOKNAME_FIELD,AUTHOR_FIELD,QUANTITY_FIELD,IMAGEURL_FIELD,DESCRIPTION_FIELD];
     add= false;
     handleAddBook(){
         this.add = true;
